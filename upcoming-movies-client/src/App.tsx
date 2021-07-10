72219bacd0574adc8d4a62b18dcf3dd7916d7ea4
Mark Kop <marcelokopmann@gmail.com>
2021-07-10 12:50:54 -0300

    feat: setup development for client + server

--- upcoming-movies-client/src/App.tsx
+++ upcoming-movies-client/src/App.tsx
@@ -1,3 +1,22 @@
-export default function App() {
-  return <h1>React App</h1>;
+import { useEffect, useState } from "react";
+
+async function getMovies() {
+  const response = await fetch('http://localhost:8080/movies/list')
+  const json = await response.json()
+  return json.data.list
+}
+
+export default function App( ) {
+  const [ movies, setMovies ] = useState([])
+
+  async function getAndSetMovies() {
+    const movies = await getMovies()
+    setMovies(movies)
+  }
+  
+  useEffect(() => {
+    getAndSetMovies()
+  }, [])
+
+  return <p>{ JSON.stringify(movies.map((movie: any) => movie.original_title), null, 2) }</p>;
 }
